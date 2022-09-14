018525561ab83465079ed6e52aa26725d1101b71
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-14 20:05:30 -0300

    Adiciona requisito 14

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const inputEmail = document.getElementById('email');
-const inputSenha = document.getElementById('password');
 const buttonLogin = document.getElementById('button-login');
+const formLogin = document.getElementById('form-login');
 
 buttonLogin.addEventListener('click', (event) => {
   event.preventDefault();
-  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+  const email = formLogin.email.value;
+  const senha = formLogin.password.value;
+
+  if (email === 'tryber@teste.com' && senha === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
