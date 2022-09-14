b8b0a80b275cd0e6305b2596eb90d7d95f0376a6
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-14 20:58:56 -0300

    Adiciona requisito 20

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttonLogin = document.getElementById('button-login');
 const formLogin = document.getElementById('form-login');
 const agreementCheckbox = document.getElementById('agreement');
 const btnSubmitForm = document.getElementById('submit-btn');
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 buttonLogin.addEventListener('click', (event) => {
   event.preventDefault();
@@ -17,4 +19,9 @@ buttonLogin.addEventListener('click', (event) => {
 
 agreementCheckbox.addEventListener('change', () => {
   btnSubmitForm.disabled = !agreementCheckbox.checked;
+});
+
+textarea.addEventListener('input', () => {
+  const leftChars = 500 - textarea.value.length;
+  counter.innerText = leftChars;
 });
