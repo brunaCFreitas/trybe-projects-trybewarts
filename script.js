ac987ff0126ada6a4869491eee757c0f3566d956
Polyana Cunha <polycunha@gmail.com>
2022-09-14 15:20:20 -0300

    requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,20 @@
+const cliqueBotao = document.querySelector(".login-button");
+const inputEmail = document.querySelector("#email");
+const inputSenha = document.querySelector("#password");
+
+function validaBotao(clique) {
+  clique.preventDefault();
+  if (
+    inputEmail.value === "tryber@teste.com" &&
+    inputSenha.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
+  } else if (
+    inputEmail.value !== "tryber@teste.com" ||
+    inputSenha.value !== "123456"
+  ) {
+    alert("Email ou senha inválidos.");
+  }
+}
+
+cliqueBotao.addEventListener("click", validaBotao);
